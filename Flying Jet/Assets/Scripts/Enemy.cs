c0c43ff8b37957a2b9f70a19d23984fddcbe850a
Sajad <sajadalipour@aut.ac.ir>
2020-10-03 21:32:38 +0330

    Scripts with Waypoint tracker

---
+++ Flying Jet/Assets/Scripts/Enemy.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
+    [SerializeField] int scorePerhit = 12;
+
+
+    [SerializeField] int hits = 3;
+
+    ScoreBoard scoreBoard;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AddBoxCollider();
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+       
+    }
+
+    private void AddBoxCollider()
+    {
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
+    private void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+        if (hits <= 0) {
+            KillEnemy();
+        }
+        
+    }
+
+    private void ProcessHit()
+    {
+        scoreBoard.ScoreHit(scorePerhit);
+        hits--;
+    }
+
+    private void KillEnemy()
+    {
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
+        Destroy(gameObject);
+    }
+}
